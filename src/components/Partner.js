3cce7d8b1e070d3c32e34da0df221ef528246db5
miketham24 <michaelthamk@gmail.com>
2024-12-01 11:04:26 -0800

    adding new sponsors

--- src/components/Partner.js
+++ src/components/Partner.js
@@ -16,27 +16,32 @@ function Partner() {
             data-aos="zoom-out-up"/>
       </div>
       <div className='companies'>
-        <Marquee pauseOnHover={true} speed={40} gradientColor={[248,251,253]}
+      <h3 data-aos="zoom-out-up">2024/2025 Partners: </h3>
+        <Marquee pauseOnHover={true} speed={50} gradientColor={[248,251,253]}
         gradientWidth={120}>
-          <img src='images/partners/sgn.jpg' alt='sgn' className='sgn2'/>
-          <img src='images/partners/vsc.jpeg' alt='vsc' className='sgn' />
-          <img src='images/partners//majulah.jpg' alt='ms' className='sgn' />
-          <img src='images/partners/tmlogo.png' alt='tm' className='sgn' />
-          <img src='images/partners/mcbc.jpg' alt='mcbc' className='sgn2'/>
-          <img src='images/partners/chatime.png' alt='chatime' className='sgn' />
-          <img src='images/partners/Indomie-Logo.png' alt='indomie' className='bc' />
-          <img src='images/partners/shopee.png' alt='shopee' className='sgn'/>
+          <img src='images/partners/sgn.jpg' alt='SGN' className='sgn2'/>
+          <img src='images/partners/yeo.png' alt='YEOs' className='mc' />
+          <img src='images/partners/chatime.png' alt='Chatime' className='chatime' />
+          <img src='images/partners/bl.png' alt='Banana Leaf' className='mc' />
+          <img src='images/partners/teadot.png' alt='TeaDot' className='sgn' />
+          <img src='images/partners/dump.png' alt='Dumpliness' className='mc'/>
+          <img src='images/partners/honey.png' alt='HoneyDew Labs' className='sgn2'/>
+          <img src='images/partners/storage-hotel.png' alt='Storage Hotel' className='mc' />
+          <img src='images/partners/rainorshine.jpg' alt='Rain or Shine' className='ros' />
         </Marquee>
+        {/* <h3 data-aos="zoom-out-up">Partners we have worked with: </h3>
         <Marquee pauseOnHover={true} speed={30} gradientColor={[248,251,253]}
         gradientWidth={120} direction={'right'}>
           <img src='images/partners/rpr.png' alt='rpr' className='mc' />
-          <img src='images/partners/yeo.png' alt='yeo' className='sgn' />
+          <img src='images/partners/tmlogo.png' alt='tm' className='sgn' />
+          <img src='images/partners/vsc.jpeg' alt='vsc' className='sgn' />
           <img src='images/partners/pb.png' alt='pb' className='mc' />
+           <img src='images/partners//majulah.jpg' alt='ms' className='sgn' />
+          <img src='images/partners/mcbc.jpg' alt='mcbc' className='sgn2'/>
           <img src='images/partners/rr.png' alt='rr' className='sgn' />
-          <img src='images/partners/bl.png' alt='bl' className='mc' />
           <img src='images/partners/nysm.png' alt='nysm' className='mc' />
-          <img src='images/partners/aseac.jpeg' alt='aseac' className='mc' />
-        </Marquee>
+          
+        </Marquee> */}
         </div>
     </div>
   );
