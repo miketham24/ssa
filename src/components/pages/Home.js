7600cacd3f397896acbf25c4f14e2952977f55d4
Maeko Elaine Notokusumo <maekoelainenotokusumo@MacBook-Air-9.local>
2025-09-05 11:36:07 -0700

    Pop up banner

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -15,12 +15,12 @@ import { Analytics } from "@vercel/analytics/react"
 function Home() {
   return (
     <>
-    {/*}
+    {
     <Announcement
-        title="NEXT EVENT: Malaysia Singapore Night (Mar 19th 6:30-9:30pm)"
-        subtitle="Join us to celebrate Singaporean and Malaysian culture!"
+        title="NEXT EVENT: Destination UBC (Sept 5th 7:00-9:00pm)"
+        subtitle="Join us for our introduction of UBC to you!"
         link="" 
-        imageSource='images/event_photos/msn23.png' // change photo
+        imageSource='images/event_photos/dest25.jpg' // change photo
         buttonText="RSVP!"
         daysToLive={1}
         secondsBeforeBannerShows={3}
@@ -28,11 +28,11 @@ function Home() {
         animateInDuration={2000}
         animateOutDuration={500}
         onBannerClick={() => {
-          window.open('https://docs.google.com/forms/d/e/1FAIpQLSfoeILK-e_6PFtA_1T1hafrjSiF6I6CRXmGXFRmt6JDGRRTpQ/viewform'); // add rsvp link here
+          window.open('https://form.jotform.com/252431416100238'); // add rsvp link here
         }
       }
       />
-      */}
+      }
       <HeroSection />
       <Analytics />
       <About />
